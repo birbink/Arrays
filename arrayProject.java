4ee985ddc23e86a2efac158359fa76e3d16b045c
birbink <50680510+birbink@users.noreply.github.com>
2019-08-07 18:05:48 -0400

    Create arrayProject.java
    Made arrays listing 10 states, their average incomes, their population, state bird, and state flower. (New York, you disappoint...) (Also why is Utah's state bird a seagull???)

---
+++ arrayProject.java
@@ -0,0 +1,5 @@
+String[] State Name = {"New York", "Hawaii", "Idaho", "Utah", "Vermont", "Florida", "California", "Colorado", "Texas", "Alaska"};
+Integer[] Average Income = {64894, 91460, 52225, 68358, 74426, 52594, 6759, 84,918, 59206, 86831};
+Integer[] Population = {1954000, 1420000, 1754000, 3161000, 626299, 2130000, 3956000, 5696000, 2870000, 737438}
+String[] State Bird = {"Eastern BLuebird", "Nene", "Mountain Bluebird", "California Gull", "Hermit Thursh", "Northern Mockingbird", "California Quail", "Lark Bunting", "Northern Mockingbird", "Alaska Willow Ptarmigan"}
+String[] State Flower = {"Rose", "Yellow Hibiscus", "Syringa", "Sego Lily", "Red Clover", "Orange Blossom", "California Poppy", "Colorado Blue Columbine", "Bluebonnet", "Forget-Me-Not"}
